d82ce2a1b97a50a32665f14d94ea8c3630a37858
nadia <nadiageras.au@gmail.com>
2023-11-09 13:45:48 +1000

    final card layout with styled components

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type StyledCardPropsType = {
+
+}
+export default function StyledCard() {
+    <div>
+        <image src={"./images/card_sunrise.jpg"}/>
+        <h3>Headline</h3>
+        <p>At ourselves direction believing departure. Celebrated her had understood projection </p>
+        <div>
+            <button type={"button"}>See more</button>
+            <button type={"button"}>Save</button>
+        </div>
+    </div>
+}
+
---
+++ src/components/CardHeadline.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledCardHeadline = styled.h3`
+  color: rgba(0, 0, 0, 0.83);
+  font-family: Inter;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: left;
+`
